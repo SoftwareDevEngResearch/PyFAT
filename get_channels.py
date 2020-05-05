29543121a6259943c216e263ecc1753e45432119
gahank <gahank@oregonstate.edu>
2020-05-05 15:52:30 -0700

    get_channels working correctly now

--- get_channels.py
+++ get_channels.py
@@ -1,37 +1,55 @@
 import pandas as pd
 
 class Channels:
+
     def __init__(self,file):
 
         def get_channels(data):
-            """Defines the column #'s for each data channel
-            Note: This really only needs to happen once, not for every file 
-            (b/c they will all be formatted the same). Update that later to 
-            only happen for the 1st file"""
-            #Get headers as a list...
-            channel_names = list(data[:0])          
-            #Define the channels needed for analysis...
-            for i in range(len(channel_names)):
-                name = channel_names[i]
-                if "Total Time" in name:
-                    time_col = name
-                elif "Position" in name:
+            """Defines the column #'s for each data channel based on the 
+            channel headers. 
+            Note: this needs to be made more universal"""
+
+            #Get file headers as a list...
+            channel_names = list(data[:0])    
+
+            for channel_name in channel_names:
+                #Change headers to standard format for identification...
+                name = channel_name.replace(" ","").lower()
+                #Use logic to identify channels...
+                if "position" in name:
                     pos_col = name
-                elif "Load" in name:
+                elif "load" in name:
                     load_col = name
-                elif "Stress" in name:
-                    stress_col = name
-                elif "Axial Strain" in name:
+                elif "axialstrain" in name:
                     ax_col = name
-                elif "Transverse Strain" in name:
+                elif "transversestrain" in name:
                     tr_col = name
+                #Some data will have a stress column, some will not...
+                if "stress" in name:
+                    stress_col = name
+                    width_col = ""
+                    thick_col = ""
+                elif "width" in name:
+                    stress_col = ""
+                    width_col = name
+                elif "thickness" in name:
+                    stress_col = ""
+                    thick_col = name
                 else:
-                    #some data will have a stress channel, some will not...
-                    stress_col = "" 
-            channels = [
-                time_col, pos_col, load_col, stress_col, ax_col, tr_col
-            ]
-            print(channels)
+                    raise ValueError(
+                        "Data must contain stress or width & thickness columns"
+                    )
+
+            channels = [pos_col, load_col, ax_col, tr_col]
+
+            #Append stress or cross-section information...
+            if stress_col=="":
+                channels.append(width_col)
+                channels.append(thick_col)
+                
+            elif width_col=="":
+                channels.append(stress_col)
+                
             return channels
 
         self.data = pd.read_csv(file,header=0)
