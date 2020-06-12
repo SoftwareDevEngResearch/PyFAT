d778c24700b93d6dff1b5eac6f8df48f6d2926fa
gahank <gahank@oregonstate.edu>
2020-06-12 13:13:07 -0700

    why is it not working?!?!?!?

--- setup.py
+++ setup.py
@@ -8,20 +8,25 @@ setuptools.setup(
     version="0.0.1",
     author="Kevan Gahan",
     author_email="gahank@oregonstate.edu",
-    license='BSD 3-clause "New" or "Revised License"',
+    license='MIT',
     description="This package analyzes .csv files created by material test systems to determine monotonic or fatigue material properties",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/SoftwareDevEngResearch/PyFAT",
     packages=setuptools.find_packages(),
+    package_dir = {
+        'pyfat':'pyfat',
+        'pyfat.tests':'pyfat/tests'
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    include_package_data=True,
     install_requires=[
-        'pytest','matplotlib','os','numpy','pandas','pathlib','argparse',
-        'datetime','csv',
+        'pytest','matplotlib','numpy','pandas','pathlib','argparse',
+        'datetime',
         ]
 )
